b6b56eccebaba9dc9126249391fc19f0bec60391
[David <[nmueller855@gmail.com]>
2023-04-13 09:48:18 -0700

    moved reducer related variables and state into a new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,8 @@
 import { useCallback, useEffect, useReducer } from 'react';
 import { getRandomSquare, calculateWinner, statusMessage } from './utils';
+import { ACTIONS, allSquaresOpen, intitialState, reducer } from './reducer';
+
 import './App.css';
-
-const ACTIONS = {
-  HANDLE_RESET: 'HANDLE_RESET',
-  CHOOSE_TEAM: 'CHOOSE_TEAM',
-  HANDLE_TURN: 'HANDLE_TURN',
-};
-
-const allSquaresOpen = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-const intitialState = {
-  chooseTeam: true,
-  team: '',
-  xIsNext: true,
-  computerTurn: false,
-  boardState: Array(9).fill(null),
-  avaialableSquareNumbers: allSquaresOpen,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTIONS.HANDLE_RESET:
-      return intitialState;
-    case ACTIONS.CHOOSE_TEAM:
-      // setChooseTeam(false);
-
-      // if (value === 'O') {
-      //   setXIsNext(false);
-      //   setTeam('O');
-      // }
-      return {
-        ...state,
-        chooseTeam: false,
-        team: action.payload.team,
-        // will xisNext be set to false if we choose 'O'
-        xIsNext: action.payload.team === 'X',
-      };
-    case ACTIONS.HANDLE_TURN: {
-      return {
-        ...state,
-        boardState: action.payload.boardState,
-        xIsNext: !state.xIsNext,
-        avaialableSquareNumbers: action.payload.avaialableSquareNumbers,
-        computerTurn: action.payload.computerTurn,
-      };
-    }
-
-    default:
-      throw new Error();
-  }
-};
 
 function App() {
   const [state, dispatch] = useReducer(reducer, intitialState);
